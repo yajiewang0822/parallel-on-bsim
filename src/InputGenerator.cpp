4de512fbdbbb749f2662e64f9b18253aa7c55123
yajiewang0822 <yajiew@andrew.cmu.edu>
2020-11-08 22:03:18 -0500

    add constructor

--- src/InputGenerator.cpp
+++ src/InputGenerator.cpp
@@ -1,1 +1,13 @@
 #include<InputGenerator.h>
+using namespace std;
+
+InputGenerator::InputGenerator(double NA, double lambda, int pattern_num, int img_size){
+  this.NA=NA;
+  this.lambda=lambda;
+  this.pattern_num=pattern_num;
+  this.img_size=img_size;
+}
+
+void InputGenerator::GenerateInputs(){
+  
+}
