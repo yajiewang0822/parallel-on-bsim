ce8ddb6c722c37a767a76ef59a15a047554a8240
yajiewang0822 <yajiew@andrew.cmu.edu>
2020-12-02 20:11:25 -0500

    start parallel

--- src/BSIM_parallel.cpp
+++ src/BSIM_parallel.cpp
@@ -6,11 +6,12 @@
  * @author: Yajie Wang
  */
 #include "InputGenerator.h"
-
 #include "helper.h"
 #include "BSIM.h"
+#include <mpi.h>
 #include <stdio.h>
 #include <ctime>
+
 
 BSIM::BSIM(int pat_num){
   this->pat_num = pat_num;
@@ -45,73 +46,12 @@ vector<vector<double> > BSIM::Reconstruction(vector<vector<vector<double> > > in
   obj=matrixScalarMul(obj, (1.0/coeffect));
 
 
-  //initial guess on the illumination patterns
-  double t_prev = 1.0;
-  double step = 1.0;
-  vector<vector<vector<double> > > patterns(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, coeffect*0.1)));
-  vector<vector<vector<double> > > patterns_next(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, 0)));
   
   
-  //fast proximal gradient descent
-  int i=0;
-  do{
-
-    //update co-effective
-    double t_curr=0.5*(1+sqrt(1+4*(t_prev*t_prev)));
-    double alpha = (t_prev-1)/t_curr;
-    t_prev = t_curr;
-
-
-    // claculate residual and the cost
-    vector<vector<vector<double> > > residual(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, 0)));
-    double cost_value = 0.0, cost_value_next = 0.0;
-    for (int j=0; j < pat_num; j++){
-      // res = input - fconv(pat*obj, psf);
-      residual[j] = matrixSub(inputs[j],fconv2(matrixEleMul(patterns[j],obj),psf));      
-      // f = sum(abs(res),3);
-      cost_value += sumImage(matrixAbs(residual[j]));
-    }
-    
-
-    // calculate gradient
-    vector<vector<double> > gradient(IMG_SIZE,vector<double>(IMG_SIZE, 0));
-    vector<vector<double> > sum_pat(IMG_SIZE, vector<double>(IMG_SIZE, 0));
-    for (int j=0; j < pat_num; j++){
-      // g = -2 * obj * fconv2(res * psf);
-      gradient = matrixScalarMul(matrixEleMul(obj, fconv2(residual[j],psf)),-2.0);
-      // pat_next = pat - g * step;
-      patterns_next[j] = matrixSub(patterns[j], matrixScalarMul(gradient, step));
-      // pat_next = pat + alpha * (pat_next - pat);
-      patterns_next[j] = matrixAdd(patterns[j], matrixScalarMul(matrixSub(patterns_next[j], patterns[j]), alpha));
-    }
-
-
-    // recalculate the residual and cost
-    for (int j=0; j < pat_num; j++){
-      // res = input - fconv(pat * obj, psf);
-      residual[j] = matrixSub(inputs[j], fconv2(matrixEleMul(patterns_next[j],obj),psf));
-      // f = sum(abs(res),3);
-      cost_value_next += sumImage(matrixAbs(residual[j]));
-    }
-
-
-    // if the cost value increases, discard the update and decrease the step
-    if (cost_value_next>cost_value){
-      printf("i: %d; discard update      ", i);
-      fflush(stdout);
-      patterns_next = patterns;
-      step = step / 2;
-    } else {
-      printf("i: %d;     ", i);
-      fflush(stdout);
-      patterns = patterns_next;
-      i++;
-    }
-  } while (i < ITER_NUM);
-
-
+  
   //covariance of inputs&patterns
   printf("begin covariance\n");
+  patterns=gradientDescent(coeffect, )
   vector<vector<double> > covar(IMG_SIZE, vector<double>(IMG_SIZE, 0));
   vector<double> input(pat_num, 0);
   vector<double> pattern(pat_num, 0);
@@ -119,12 +59,16 @@ vector<vector<double> > BSIM::Reconstruction(vector<vector<vector<double> > > in
     for (int j = 0; j < IMG_SIZE; j++){
       double mean_input = 0.0;
       double mean_pat = 0.0;
+      // MPI TODO
+      // 1. Each processor added all local patterns based on pixels and return the sum array(256*256)
+      // 2. Master added the returned sum array from each processor based on pixels
       for (int k = 0; k < this->pat_num; k++){
         input[k] = inputs[k][i][j];
         pattern[k] = patterns[k][i][j];
         mean_input += input[k];
         mean_pat += pattern[k];
       }
+      // Same with sequential: calculate the covariance of input and pattern based on pixels
       mean_input /= this->pat_num;
       mean_pat /= this->pat_num;
       for (int k = 0; k < this->pat_num; k++){
@@ -137,6 +81,84 @@ vector<vector<double> > BSIM::Reconstruction(vector<vector<vector<double> > > in
   }
 
   return covar;
+}
+
+vector<vector<vector<double> > > gradientDescent(, double coeffect, vector<vector<vector<double> > > inputs, vector<vector<double> > psfn, vector<vector<double> > psf){
+//initial guess on the illumination patterns
+  double t_prev = 1.0;
+  double step = 1.0;
+  vector<vector<vector<double> > > patterns(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, coeffect*0.1)));
+  vector<vector<vector<double> > > patterns_next(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, 0)));
+  
+//fast proximal gradient descent
+// Move Part A out of while loop, make it a variable cost_prev
+//
+// Concerns: parallel work may prevent gradient descending
+//
+// MPI process:
+// 1. master does work/ master does not do work
+// 2. Divide num_patterns based on num_processes, assign patterns to each, locally generate 
+// 3. Each processor runs Part B, C, D, Send the cost value in D if cost value is small enough.
+
+
+//master: array[p1v, p2v, p3v...]
+  int i=0;s
+  do{
+
+    //update coefficient
+    double t_curr=0.5*(1+sqrt(1+4*(t_prev*t_prev)));
+    double alpha = (t_prev-1)/t_curr;
+    t_prev = t_curr;
+
+
+    // Part A claculate residual and the cost
+    vector<vector<vector<double> > > residual(pat_num, vector<vector<double> >(IMG_SIZE, vector<double>(IMG_SIZE, 0)));
+    double cost_value = 0.0, cost_value_next = 0.0;
+    for (int j=0; j < pat_num; j++){
+      // res = input - fconv(pat*obj, psf);
+      residual[j] = matrixSub(inputs[j],fconv2(matrixEleMul(patterns[j],obj),psf));      
+      // f = sum(abs(res),3);
+      cost_value += sumImage(matrixAbs(residual[j]));
+    }
+    
+
+    // Part B calculate gradient
+    vector<vector<double> > gradient(IMG_SIZE,vector<double>(IMG_SIZE, 0));
+    vector<vector<double> > sum_pat(IMG_SIZE, vector<double>(IMG_SIZE, 0));
+    
+    for (int j=0; j < pat_num; j++){
+      // g = -2 * obj * fconv2(res * psf);
+      gradient = matrixScalarMul(matrixEleMul(obj, fconv2(residual[j],psf)),-2.0);
+      // pat_next = pat - g * step;
+      patterns_next[j] = matrixSub(patterns[j], matrixScalarMul(gradient, step));
+      // pat_next = pat + alpha * (pat_next - pat);
+      patterns_next[j] = matrixAdd(patterns[j], matrixScalarMul(matrixSub(patterns_next[j], patterns[j]), alpha));
+    }
+
+
+    // Part C recalculate the residual and cost
+    for (int j=0; j < pat_num; j++){
+      // res = input - fconv(pat * obj, psf);
+      residual[j] = matrixSub(inputs[j], fconv2(matrixEleMul(patterns_next[j],obj),psf));
+      // f = sum(abs(res),3);
+      cost_value_next += sumImage(matrixAbs(residual[j]));
+    }
+
+
+    // Part D if the cost value increases, discard the update and decrease the step
+    if (cost_value_next>cost_value){
+      printf("i: %d; discard update      ", i);
+      fflush(stdout);
+      patterns_next = patterns;
+      step = step / 2;
+    } else {
+      printf("i: %d;     ", i);
+      fflush(stdout);
+      patterns = patterns_next;
+      i++;
+    }
+  } while (i < ITER_NUM);
+  return patterns;
 }
 
 int main()
